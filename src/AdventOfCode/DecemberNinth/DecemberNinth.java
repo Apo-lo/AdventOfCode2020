8f40c40dd0e31525fe76a9b77c376271c307ab08
FH <niklas.fleig@hs-furtwangen.de>
2020-12-19 13:58:18 +0100

    solutions for day nine puzzle two

--- src/AdventOfCode/DecemberNinth/DecemberNinth.java
+++ src/AdventOfCode/DecemberNinth/DecemberNinth.java
@@ -4,6 +4,8 @@ import AdventOfCode.AdventOfCodeGlobals;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class DecemberNinth {
@@ -16,27 +18,45 @@ public class DecemberNinth {
     }
 
     private static void solvePuzzleTwo() {
+        List<BigInteger> result = calculateListOfNumbers();
+        System.out.println("Sum of max and min number in list: " + (Collections.max(result).add(Collections.min(result))));
+    }
+    private static List<BigInteger> calculateListOfNumbers() {
+        BigInteger invalidNumber = BigInteger.valueOf(41682220);
+        ArrayList<BigInteger> input = inputsForDecemberNinth();
+        BigInteger sum = BigInteger.valueOf(0);
+        List<BigInteger> numberOfSum = new ArrayList<>();
 
-    /*ToDo
-       while all sum < my number -> sum + next number (index in loop) if sum == my Number
-       save numbers in list return list min * max
-     */
-
-
+        for (int i = 0; i < input.size(); i++) {
+            sum = sum.add(input.get(i));
+            numberOfSum.add(input.get(i));
+            for (int j = i + 1; j < input.size(); j++) {
+                sum = sum.add(input.get(j));
+                numberOfSum.add(input.get(j));
+                if(sum.equals(invalidNumber) && numberOfSum.size() >= 2) {
+                    return numberOfSum;
+                } else if (sum.compareTo(invalidNumber) > 0) {
+                    break;
+                }
+            }
+            sum = BigInteger.valueOf(0);
+            numberOfSum = new ArrayList<>();
+        }
+        return numberOfSum;
     }
 
     private static void solvePuzzleOne() {
         ArrayList<BigInteger> input = inputsForDecemberNinth();
 
         for(int i = 25; i < input.size(); i++) {
-            if(!sumUp(input.subList(i - 25, i), input.get(i))) {
+            if(!sumOfTwo(input.subList(i - 25, i), input.get(i))) {
                 System.out.println("First number that does not sum up: " + input.get(i));
                 break;
             }
         }
     }
 
-    private static boolean sumUp(List<BigInteger> previousNumbers, BigInteger numberToCheck) {
+    private static boolean sumOfTwo(List<BigInteger> previousNumbers, BigInteger numberToCheck) {
         int sizeOfList = previousNumbers.size();
         for (int i = 0; i < sizeOfList; i++) {
             for (BigInteger previousNumber : previousNumbers) {
@@ -50,5 +70,9 @@ public class DecemberNinth {
 
     private static ArrayList<BigInteger> inputsForDecemberNinth() {
         return AdventOfCodeGlobals.getInputsAsBigIntegers("src/AdventOfCode/DecemberNinth/DecemberNinthInput.txt");
+    }
+
+    private static ArrayList<BigInteger> testInputsForDecemberNinth() {
+        return AdventOfCodeGlobals.getInputsAsBigIntegers("src/AdventOfCode/DecemberNinth/DecemberNinthTestInput.txt");
     }
 }
---
+++ src/AdventOfCode/DecemberTenth/DecemberTenth.java
@@ -0,0 +1,26 @@
+package AdventOfCode.DecemberTenth;
+
+import AdventOfCode.AdventOfCodeGlobals;
+
+import java.util.ArrayList;
+
+public class DecemberTenth {
+    public static void main (String[] args) {
+        System.out.println("--------- Puzzle One ----------");
+        solvePuzzleOne();
+        System.out.println("--------- Puzzle Two ----------");
+        solvePuzzleTwo();
+        System.out.println("-------------------------------");
+    }
+
+    private static void solvePuzzleTwo() {
+    }
+
+    private static void solvePuzzleOne() {
+
+    }
+
+    private static ArrayList<Integer> inputsForDecemberTenth() {
+        return AdventOfCodeGlobals.getInputsAsIntegers("src/AdventOfCode/DecemberTenth/DecemberTenthInput.txt");
+    }
+}

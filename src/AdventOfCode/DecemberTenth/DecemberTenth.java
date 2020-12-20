0cb63fcb6c6a7b049861f6a344897d627261946e
FH <niklas.fleig@hs-furtwangen.de>
2020-12-20 16:32:42 +0100

    solutions for day ten puzzle two

--- src/AdventOfCode/DecemberTenth/DecemberTenth.java
+++ src/AdventOfCode/DecemberTenth/DecemberTenth.java
@@ -4,9 +4,12 @@ import AdventOfCode.AdventOfCodeGlobals;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DecemberTenth {
     static int myDevicesJolts = Collections.max(inputsForDecemberTenth()) + 3;
+    static int numberOfPermutations = 0;
 
     public static void main (String[] args) {
         System.out.println("--------- Puzzle One ----------");
@@ -17,12 +20,12 @@ public class DecemberTenth {
     }
 
     private static void solvePuzzleTwo() {
+        Map<Integer, Long> memoizationMap = new HashMap<>();
+        System.out.println("Number of valid permutations: " + dynamicApproach(0, memoizationMap, inputsForDecemberTenth()));
     }
 
     private static void solvePuzzleOne() {
         ArrayList<Integer> input = inputsForDecemberTenth();
-        input.add(0); //starting jolt value
-        Collections.sort(input);
 
         int adaptersWithOneDifference = 0;
         int adaptersWithThreeDifference = 0;
@@ -43,10 +46,29 @@ public class DecemberTenth {
         System.out.println("Number of one jolt difference times jolts of three difference " + (adaptersWithOneDifference * adaptersWithThreeDifference));
     }
 
+    public static long dynamicApproach(int i, Map<Integer, Long> memoizationMap, ArrayList<Integer> inputs) {
+        long result = 0;
+        if (i == inputs.size() - 1) {
+            return 1;
+        }
+        if (memoizationMap.containsKey(i)) {
+            return memoizationMap.get(i);
+        }
+        for (int j = i + 1; j < inputs.size(); j++) {
+            if ((inputs.get(j) - inputs.get(i)) <= 3) {
+                result += dynamicApproach(j, memoizationMap, inputs);
+            }
+        }
+        memoizationMap.put(i, result);
+        return result;
+    }
 
 
     private static ArrayList<Integer> inputsForDecemberTenth() {
-        return AdventOfCodeGlobals.getInputsAsIntegers("src/AdventOfCode/DecemberTenth/DecemberTenthInput.txt");
+        ArrayList<Integer> input = AdventOfCodeGlobals.getInputsAsIntegers("src/AdventOfCode/DecemberTenth/DecemberTenthInput.txt");
+        input.add(0);
+        Collections.sort(input);
+        return input;
     }
 
     private static ArrayList<Integer> testInputsForDecemberTenth() {
